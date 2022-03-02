b9becec6b08094e9e5e470f497dbc667c76b51d1
David Maisonave <47364845+David-Maisonave@users.noreply.github.com>
2022-03-02 16:41:13 -0500

    Update AssemblyInfo.cs

--- PasteIntoFile/Properties/AssemblyInfo.cs
+++ PasteIntoFile/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasteIntoFile")]
+[assembly: AssemblyTitle("Paste Clipboard Contents Into File")]
 [assembly: AssemblyDescription("Paste Clipboard Contents Into Files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EslaMxSoft 2022")]
-[assembly: AssemblyProduct("PasteIntoFile v1.6")]
+[assembly: AssemblyProduct("PasteIntoFile + David Maisonave Enhancements")]
 [assembly: AssemblyCopyright("Copyright © EslaMxSoft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.1")]
-[assembly: AssemblyFileVersion("1.6.0.1")]
+[assembly: AssemblyVersion("1.6.0.2")]
+[assembly: AssemblyFileVersion("1.6.0.2")]
